65158756014709672069c35fe9bcf2fe907de2d6
dicki <dickiclever@gmail.com>
2022-11-02 13:47:36 +0700

    add admin index section 1 on landing page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -20,4 +20,33 @@
         </div>
     </div>
 </div>
+<div class="admin-index-sect-1">
+    <div class="content">
+        <div class="sect-1-container">
+            <h1>Layanan Apadok</h1>
+            <div class="ui grid">
+                <div class="four wide column">
+                    <img src="{{ asset('images/monitorDataImg.png') }}" alt="monitorData" srcset="">
+                    <h2>Monitor Data</h2>
+                    <p>Monitor data pasien anda melalui website Apadok khusus admin klinik</p>
+                </div>
+                <div class="four wide column">
+                    <img src="{{ asset('images/lihatPasienImg.png') }}" alt="lihatPasien" srcset="">
+                    <h2>Lihat Pasien</h2>
+                    <p>Lihat daftar pasien serta grafik hasil tes setiap pasien secara lengkap dan akurat</p>
+                </div>
+                <div class="four wide column">
+                    <img src="{{ asset('images/konsultasiKlinikImg.png') }}" alt="konsultasiKlinik" srcset="">
+                    <h2>Konsultasi Klinik</h2>
+                    <p>Apadok memberikan layanan konsultasi antara klinik dan pasien melalui fitur chat pada mobile app</p>
+                </div>
+                <div class="four wide column">
+                    <img src="{{ asset('images/tesKesehatanImg.png') }}" alt="tesKesehatan" srcset="">
+                    <h2>Tes Kesehatan</h2>
+                    <p>Lakukan tes skrining kesehatan untuk mengetahui status kesehatan anda</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
