0a09be883ddebff2f7849d02f9b6a83b8b8f3216
dicki <dickiclever@gmail.com>
2022-11-09 12:08:12 +0700

    rework on login page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,98 +7,90 @@
 @section('main')
 <div class="landing-container">
     <div class="landing-jumbotron">
-        <div class="content">
-            <div class="ui vertically divided grid">
-                <div class="two column row">
-                    <div class="column">
-                        <div class="left-side">
-                            <h1>Layanan Kesehatan Tepat Bersama Apadok</h1>
-                            <p>Apadok membantu klinik-klinik mengembangkan serta mengelola bisnisnya secara tepat dan akurat.</p>
-                            <button class="ui orange button">Daftar Sekarang</button>
-                        </div>
+        <div class="ui vertically divided grid">
+            <div class="two column row">
+                <div class="column">
+                    <div class="left-side">
+                        <h1>Layanan Kesehatan Tepat Bersama Apadok</h1>
+                        <p>Apadok membantu klinik-klinik mengembangkan serta mengelola bisnisnya secara tepat dan akurat.</p>
+                        <button class="ui orange button">Daftar Sekarang</button>
                     </div>
-                    <div class="column">
-                        <div class="right-side">
-                            <img src="{{ asset('images/doctorVect.png') }}" alt="doctorVect" srcset="">
-                        </div>
+                </div>
+                <div class="column">
+                    <div class="right-side">
+                        <img src="{{ asset('images/doctorVect.png') }}" alt="doctorVect" srcset="">
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <div class="landing-sect-1">
-        <div class="content">
-            <div class="sect-1-container">
-                <h1>Layanan Apadok</h1>
-                <div class="ui four column grid">
-                    <div class="column">
-                        <img src="{{ asset('images/monitorDataImg.png') }}" alt="monitorData" srcset="">
-                        <h2>Monitor Data</h2>
-                        <p>Monitor data pasien anda melalui website Apadok khusus admin klinik</p>
-                    </div>
-                    <div class="column">
-                        <img src="{{ asset('images/lihatPasienImg.png') }}" alt="lihatPasien" srcset="">
-                        <h2>Lihat Pasien</h2>
-                        <p>Lihat daftar pasien serta grafik hasil tes setiap pasien secara lengkap dan akurat</p>
-                    </div>
-                    <div class="column">
-                        <img src="{{ asset('images/konsultasiKlinikImg.png') }}" alt="konsultasiKlinik" srcset="">
-                        <h2>Konsultasi Klinik</h2>
-                        <p>Apadok memberikan layanan konsultasi antara klinik dan pasien melalui fitur chat pada mobile app</p>
-                    </div>
-                    <div class="column">
-                        <img src="{{ asset('images/tesKesehatanImg.png') }}" alt="tesKesehatan" srcset="">
-                        <h2>Tes Kesehatan</h2>
-                        <p>Lakukan tes skrining kesehatan untuk mengetahui status kesehatan anda</p>
-                    </div>
+        <div class="sect-1-container">
+            <h1>Layanan Apadok</h1>
+            <div class="ui four column grid">
+                <div class="column">
+                    <img src="{{ asset('images/monitorDataImg.png') }}" alt="monitorData" srcset="">
+                    <h2>Monitor Data</h2>
+                    <p>Monitor data pasien anda melalui website Apadok khusus admin klinik</p>
+                </div>
+                <div class="column">
+                    <img src="{{ asset('images/lihatPasienImg.png') }}" alt="lihatPasien" srcset="">
+                    <h2>Lihat Pasien</h2>
+                    <p>Lihat daftar pasien serta grafik hasil tes setiap pasien secara lengkap dan akurat</p>
+                </div>
+                <div class="column">
+                    <img src="{{ asset('images/konsultasiKlinikImg.png') }}" alt="konsultasiKlinik" srcset="">
+                    <h2>Konsultasi Klinik</h2>
+                    <p>Apadok memberikan layanan konsultasi antara klinik dan pasien melalui fitur chat pada mobile app</p>
+                </div>
+                <div class="column">
+                    <img src="{{ asset('images/tesKesehatanImg.png') }}" alt="tesKesehatan" srcset="">
+                    <h2>Tes Kesehatan</h2>
+                    <p>Lakukan tes skrining kesehatan untuk mengetahui status kesehatan anda</p>
                 </div>
             </div>
         </div>
     </div>
     <div class="landing-sect-2">
-        <div class="content">
-            <div class="sect-2-container">
-                <h1>Mengapa Bergabung Bersama Apadok</h1>
-                <div class="ui three column grid">
-                    <div class="column box-column">
-                        <h2>Sistem yang Terintegrasi</h2>
-                        <p>Dengan Apadok, klinik anda memiliki sistem terpadu serta terintegrasi dan dapat diakses secara daring. Apadok dirancang untuk memenuhi kebutuhan klinik anda</p>
-                    </div>
-                    <div class="column box-column mid-column">
-                        <h2>Standarisasi Layanan</h2>
-                        <p>Layanan klinik anda akan distandarisasi sesuai dengan standar Apadok guna meningkatkan kualitas klinik anda</p>
-                    </div>
-                    <div class="column box-column">
-                        <h2>Dimanapun dan Kapanpun</h2>
-                        <p>Akses website maupun mobile app Apadok kapanpun dan dimanapun setiap harinya</p>
-                    </div>
+        <div class="sect-2-container">
+            <h1>Mengapa Bergabung Bersama Apadok</h1>
+            <div class="ui three column grid">
+                <div class="column box-column">
+                    <h2>Sistem yang Terintegrasi</h2>
+                    <p>Dengan Apadok, klinik anda memiliki sistem terpadu serta terintegrasi dan dapat diakses secara daring. Apadok dirancang untuk memenuhi kebutuhan klinik anda</p>
+                </div>
+                <div class="column box-column mid-column">
+                    <h2>Standarisasi Layanan</h2>
+                    <p>Layanan klinik anda akan distandarisasi sesuai dengan standar Apadok guna meningkatkan kualitas klinik anda</p>
+                </div>
+                <div class="column box-column">
+                    <h2>Dimanapun dan Kapanpun</h2>
+                    <p>Akses website maupun mobile app Apadok kapanpun dan dimanapun setiap harinya</p>
                 </div>
             </div>
         </div>
     </div>
     <div class="landing-sect-3">
-        <div class="content">
-            <div class="sect-3-container">
-                <h1>Artikel Kesehatan</h1>
-                <div class="ui three column centered grid box-grid">
-                    <div class="column box-column">
-                        <img src="{{ asset('images/headImg.png') }}" alt="head" srcset="">
-                        <p>Trik psikologis untuk membuat anda semakin bugar</p>
-                        <a href="">Baca selengkapnya</a>
-                    </div>
-                    <div class="column box-column mid-column">
-                        <img src="{{ asset('images/situpImg.png') }}" alt="situp" srcset="">
-                        <p>Ini 5 manfaat sit-up untuk kesehatan jangka panjang</p>
-                        <a href="">Baca selengkapnya</a>
-                    </div>
-                    <div class="column box-column">
-                        <img src="{{ asset('images/bananaImg.png') }}" alt="banana" srcset="">
-                        <p>Kenali nutrisi Pisang sebelum Anda memakannya secara rutin</p>
-                        <a href="">Baca selengkapnya</a>
-                    </div>
+        <div class="sect-3-container">
+            <h1>Artikel Kesehatan</h1>
+            <div class="ui three column centered grid box-grid">
+                <div class="column box-column">
+                    <img src="{{ asset('images/headImg.png') }}" alt="head" srcset="">
+                    <p>Trik psikologis untuk membuat anda semakin bugar</p>
+                    <a href="">Baca selengkapnya</a>
                 </div>
-                <button class="ui orange button">Lihat Semua</button>
+                <div class="column box-column mid-column">
+                    <img src="{{ asset('images/situpImg.png') }}" alt="situp" srcset="">
+                    <p>Ini 5 manfaat sit-up untuk kesehatan jangka panjang</p>
+                    <a href="">Baca selengkapnya</a>
+                </div>
+                <div class="column box-column">
+                    <img src="{{ asset('images/bananaImg.png') }}" alt="banana" srcset="">
+                    <p>Kenali nutrisi Pisang sebelum Anda memakannya secara rutin</p>
+                    <a href="">Baca selengkapnya</a>
+                </div>
             </div>
+            <button class="ui orange button">Lihat Semua</button>
         </div>
     </div>
 </div>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,20 +9,20 @@
     <div class="form-container">
         <div class="ui two column grid">
             <div class="column left-column">
-                <img src="{{ asset('images/doctVectLogin.png') }}" alt="doctorVectLogin" srcset="">
+                <img src="{{ asset('images/login/doctLogin.png') }}" alt="doctorVectLogin" srcset="">
             </div>
             <div class="column right-column">
                 <h1>Masuk</h1>
                 <form class="ui large form">
                     <div class="field">
                         <p>Username</p>
-                        <input type="text" name="email" placeholder="E-mail address">
+                        <input type="text" name="email" placeholder="Masukkan username anda">
                     </div>
                     <div class="field">
                         <p>Password</p>
-                        <input type="password" name="password" placeholder="Password">
+                        <input type="password" name="password" placeholder="Masukkan password anda">
                     </div>
-                    <button class="ui fluid large orange submit button" type="submit">Lanjutkan</button>
+                    <button class="ui fluid large blue submit button" type="submit">Lanjutkan</button>
                 </form>
             </div>
         </div>
